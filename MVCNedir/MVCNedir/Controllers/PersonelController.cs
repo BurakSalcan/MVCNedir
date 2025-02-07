c89fe67e61901f1ba18cba88806b5df39c11047e
Burak SALCAN <buraksalcan@gmail.com>
2025-02-07 16:55:48 +0300

    26.01.2025

---
+++ MVCNedir/MVCNedir/Controllers/PersonelController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCNedir.Models;
+
+
+namespace MVCNedir.Controllers
+{
+    //Controller
+    public class PersonelController : Controller
+    {
+        //Action
+
+        //Action result olmadan view olsada çalışmaz
+        public ActionResult Listele() //metoda sağ klik add view empty ve layoutsuz
+        {
+            return View();
+        }
+        public ActionResult VeriAktarim()
+        {
+            ViewBag.deneme = "Selamlar ben Samet yazılımcı olmaya çalışıyorum"; //en sık kullanılan
+            ViewData["deneme2"] = "Merhaba ben viewdata";
+            TempData["data"] = "tempdataaaaaaaa";
+            return RedirectToAction("Listele", "Personel");
+        }
+        public ActionResult ModelIleVeriAktarimi()
+        {
+            Personel p = new Personel();
+            p.ID = 1;
+            p.Isim = "Murtaza";
+            p.Soyisim = "Şuayipoğlu";
+            return View(p);
+        }
+        [HttpGet]
+        public ActionResult VeriAlalim()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult VeriAlalim(string isim, string soyisim)
+        {
+            Personel p = new Personel();
+            p.Isim = isim;
+            p.Soyisim = soyisim;
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult ModelIleVeriAlalim()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ModelIleVeriAlalim(Personel p)
+        {
+
+            return View();
+        }
+    }
+}
---
+++ MVCNedir/MVCNedir/Models/Personel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCNedir.Models
+{
+    public class Personel
+    {
+        public int ID { get; set; }
+        public string Isim { get; set; }
+        public string Soyisim { get; set; }
+
+    }
+}
